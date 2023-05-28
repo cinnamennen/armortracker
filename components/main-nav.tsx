e22d82e588535a9eff55a1fd26e771fe2df23ca5
Caleb Mennen <2693113+cinnamennen@users.noreply.github.com>
2023-05-28 13:24:45 -0400

    try to shove buttons into a menu

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,31 +1,109 @@
 "use client"
 
 import * as React from "react"
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react"
 import Link from "next/link"
+import {Download, LucideIcon, Mail, Upload} from "lucide-react"
 
 import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
 import {
-  NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
 export function MainNav() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        {siteConfig.mainNav.map(({ href, title }) => (
-          <NavigationMenuItem key={title}>
-            <Link href={href} legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                {title}
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+    <NavigationMenuList>
+      {siteConfig.mainNav.map(({ href, title }) => (
+        <NavigationMenuItem key={title}>
+          <Link href={href} legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              {title}
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      ))}
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Settings</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            {components.map((component) => (
+              <ListItem
+                key={component.title}
+                {...component}
+              >
+                {component.description}
+              </ListItem>
+            ))}
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    </NavigationMenuList>
   )
 }
+
+const components: {
+  title: string
+  href?: string
+  description: string
+  Icon?: LucideIcon
+  onClick?: () => void
+}[] = [
+  {
+    title: "Export",
+    description:
+      "Export your armor and inventory to your clipboard so that you can load it on another device.",
+    Icon: Upload,
+    onClick: () => {console.log("clicked")}
+  },
+  {
+    title: "Import",
+    description:
+      "Import your armor and inventory data from your clipboard.",
+    Icon: Download,
+  },
+  {
+    title: "Give Feedback",
+    href: siteConfig.links.feedback,
+    Icon: Mail,
+
+    description:
+      "Open a issue in GitHub to provide feedback.",
+  },
+]
+
+const ListItem = forwardRef<
+  ElementRef<"a">,
+  ComponentPropsWithoutRef<"a"> & { Icon?: LucideIcon }
+>(({ className, title, Icon, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:cursor-pointer hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="inline-flex items-end text-sm font-medium leading-none">
+            <>
+              {Icon && <Icon className="h-4 w-4 mr-2"/>}
+              {title}
+            </>
+          </div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link"
 import { Github, Mail } from "lucide-react"
 
@@ -7,25 +8,27 @@ import Export from "@/components/Export"
 import Import from "@/components/Import"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import {NavigationMenu} from "@/components/ui/navigation-menu";
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <NavigationMenu>
         <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <Import />
-            <Export />
-            <Link
-              href={siteConfig.links.feedback}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Button variant="ghost" size="sm">
-                <Mail className="mr-2 h-4 w-4" /> Give Feedback
-              </Button>
-            </Link>
+            {/*<Import />*/}
+            {/*<Export />*/}
+            {/*<Link*/}
+            {/*  href={siteConfig.links.feedback}*/}
+            {/*  target="_blank"*/}
+            {/*  rel="noreferrer"*/}
+            {/*>*/}
+            {/*  <Button variant="ghost" size="sm">*/}
+            {/*    <Mail className="mr-2 h-4 w-4" /> Give Feedback*/}
+            {/*  </Button>*/}
+            {/*</Link>*/}
             <Link
               href={siteConfig.links.github}
               target="_blank"
@@ -40,6 +43,7 @@ export function SiteHeader() {
             <ThemeToggle />
           </nav>
         </div>
+          </NavigationMenu>
       </div>
     </header>
   )
