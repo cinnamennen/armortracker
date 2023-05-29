1fddbaf4862507ceb54c7da14d91919a1ad8f811
Caleb Mennen <2693113+cinnamennen@users.noreply.github.com>
2023-05-29 10:21:55 -0400

    add components

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,6 +12,7 @@ import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 import { Icons } from "./icons"
+import {MobileNav} from "@/components/mobile-nav";
 
 export function SiteHeader() {
   return (
@@ -25,6 +26,7 @@ export function SiteHeader() {
             </span>
           </div>
           <MainNav />
+          <MobileNav />
           <div className="flex flex-1 items-center justify-end space-x-4">
             <nav className="flex items-center space-x-1">
               <Link
