97e0cfd88c252858a528a14dfec0e591a131a0a4
Caleb Mennen <2693113+cinnamennen@users.noreply.github.com>
2023-05-29 13:01:56 -0400

    update links

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,11 @@
 export const siteConfig = {
   name: "TotK Armor Planner",
   url: "https://totkarmortracker.com",
-  ogImage: "https://ui.shadcn.com/og.jpg",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   links: {
-    feedback: "https://github.com/shadcn/ui",
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
+    feedback: "https://github.com/cinnamennen/armortracker/issues/new/choose",
+    github: "https://github.com/cinnamennen/armortracker",
   },
 }
 
