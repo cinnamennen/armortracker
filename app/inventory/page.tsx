806c2bf4b1fc29c4b7717f12a1cf51eaefe8b853
Caleb Mennen <2693113+cinnamennen@users.noreply.github.com>
2023-06-01 13:14:03 -0400

    cleanup

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -5,7 +5,7 @@ import { IngredientCard } from "@/app/inventory/ingredientCard"
 
 export default function IngredientDisplay() {
   return (
-    <div className="grid grid-cols-2 gap-4 sm:grid-cols-4">
+    <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 xl:grid-cols-6">
       {Object.values(Ingredient)
         .filter(isNotString)
         .map((ingredient, index) => (
