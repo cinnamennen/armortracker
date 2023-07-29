5a850570c9ab5ecbebad7f63e1a8aba3f0071473
Caleb Mennen <2693113+cinnamennen@users.noreply.github.com>
2023-07-29 15:56:16 -0700

    Update FAQ (#64)
    * Fix #60
    
    * Fix #60
    
    * update FAQ

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -4,21 +4,11 @@ export default function FAQ() {
   return (
     <div>
       <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        Some of the images aren&apos;t loading!
-      </h2>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
-        You&apos;re right! Currently most of the images are sourced from{" "}
-        <Link href="zeldawiki.wiki">zeldawiki.wiki</Link> and currently they
-        don&apos;t have images for everything yet. As more images are available
-        the site will be updated to reflect that.
-      </p>
-      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         A recipe is wrong!
       </h2>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
-        A best effort was made to ensure that all recipes are correct, but there
-        are currently conflicting reports on some recipes. If you find a recipe
-        that is wrong, please let me know by{" "}
+        A best effort was made to ensure that all recipes are correct. If you
+        find a recipe that is wrong, please let me know by{" "}
         <Link
           href="https://github.com/cinnamennen/armortracker/issues/new"
           className="font-medium text-primary underline underline-offset-4"
@@ -26,7 +16,7 @@ export default function FAQ() {
           opening an issue on GitHub
         </Link>
         . When you do so please include some sort of citation for the recipe,
-        whether that&amp;s some links to other sites with the recipe or a
+        whether that&apos;s some links to other sites with the recipe or a
         screenshot of the recipe in game.
       </p>
     </div>
